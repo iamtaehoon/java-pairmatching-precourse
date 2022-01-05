1f2cc5fbfeef97fbd65991902f2e3f7e7474dacc
iamtaehoon <kimth9981@naver.com>
2022-01-05 16:12:06 +0900

    feat(): 크루들 결과 출력 기능 구현

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -8,8 +8,17 @@ public class OutputView {
     }
 
     public static void showResult(List<String> thisProgramsPair) {
-        for (String pair : thisProgramsPair) {
-            System.out.println("pair = " + pair);
+        for (int i = 0; i < thisProgramsPair.size() / 2 -1; i++) {
+            System.out.println(thisProgramsPair.get(i*2) + " : " + thisProgramsPair.get(i*2 + 1));
         }
+        if (thisProgramsPair.size() % 2 == 0) {
+            System.out.println(thisProgramsPair.get(thisProgramsPair.size() - 2) + " : " + thisProgramsPair.get(
+                thisProgramsPair.size() - 1));
+        }
+        if (thisProgramsPair.size() % 2 != 0) {
+            System.out.println(thisProgramsPair.get(thisProgramsPair.size() - 3) + " : " + thisProgramsPair.get(
+                thisProgramsPair.size() - 2) + " : " + thisProgramsPair.get(thisProgramsPair.size() - 1));
+        }
+
     }
 }
