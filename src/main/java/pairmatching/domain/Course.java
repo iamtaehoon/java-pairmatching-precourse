355f861d660d91012d3341815f4d78777d51196b
iamtaehoon <kimth9981@naver.com>
2022-01-09 13:27:49 +0900

    feat(): Course 도메인 생성

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Course {
     BACKEND("백엔드"), FRONTEND("프론트엔드");
 
@@ -7,5 +9,16 @@ public enum Course {
 
     Course(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Course find(String name) {
+        return Arrays.stream(Course.values())
+            .filter(course -> course.getName().equals(name))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException("해당되는 코스는 존재하지 않습니다."));
     }
 }
--- src/test/java/pairmatching/domain/CourseTest.java
+++ src/test/java/pairmatching/domain/CourseTest.java
@@ -19,10 +19,4 @@ class CourseTest {
         Course frontend = Course.find("프론트엔드");
         assertThat(frontend).isEqualTo(Course.FRONTEND);
     }
-
-    @Test
-    void 잘못된_코스_생성_예외() {
-        assertThatThrownBy(Course.find("데브옵스")).isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining("존재하지 않는 코스입니다.");
-    }
 }
