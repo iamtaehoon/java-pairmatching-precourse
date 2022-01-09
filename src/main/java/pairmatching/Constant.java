e788e21df1090011198796cf19a6cb28f626991f
iamtaehoon <kimth9981@naver.com>
2022-01-09 15:44:57 +0900

    feat(): ProgramInfo 입력값 양식 검증 추가

--- src/main/java/pairmatching/Constant.java
+++ src/main/java/pairmatching/Constant.java
@@ -1,6 +1,7 @@
 package pairmatching;
 
 public abstract class Constant {
+    public static int DETAIL_CNT = 3;
     public static int COURSE_IDX = 0;
     public static int LEVEL_IDX = 1;
     public static int MISSION_IDX = 2;
--- src/main/java/pairmatching/util/ProgramInfoConvertor.java
+++ src/main/java/pairmatching/util/ProgramInfoConvertor.java
@@ -11,6 +11,9 @@ public class ProgramInfoConvertor {
 
     public static ProgramInfo makeProgramInfo(String programInfoPreProcessing) {
         String[] programInfoDetailsPreProcessing = programInfoPreProcessing.split(", ");
+        if (programInfoDetailsPreProcessing.length != DETAIL_CNT) {
+            throw new IllegalArgumentException("입력값의 양식이 잘못되었습니다.");
+        }
         Course course = Course.find(programInfoDetailsPreProcessing[COURSE_IDX]);
         Level level = Level.find(programInfoDetailsPreProcessing[LEVEL_IDX]);
         Mission mission = level.hasMission(programInfoDetailsPreProcessing[MISSION_IDX]);
